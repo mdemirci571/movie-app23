a4d33d9c95418a13da678495f61f0016fd8396a2
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 20:45:34 +0100

    v15

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -47,32 +47,38 @@ const MovieDetail = () => {
   }, [movieDetailBaseUrl, videoUrl]);
 
   return (
-    <div className="md:container px-10 mx-auto py-5">
-      <h1 className="text-center text-white text-3xl">{title}</h1>
+    <div className="md:container px-10 mx-auto py-5 dark:text-white text-center">
+      <div className="mb-[3rem] ">
+        <h1 className="text-4xl mb-5">{title}</h1>
+        {tagline && <h3 className="text-2xl ">"{tagline}"</h3>}
+      </div>
       {videoKey && <VideoSection videoKey={videoKey} />}
       <div className="md:container flex justify-center px-10">
-        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg">
+        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg mb-14">
           <img
             className=" lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
             src={poster_path ? baseImageUrl + poster_path : defaultImage}
             alt="poster"
           />
-          <div className="p-6 flex flex-col justify-between">
-            <div>
-              <h5 className="text-gray-900 text-xl font-medium mb-2">
+          <div className="p-6 flex flex-col justify-between dark:bg-gray-900 ">
+            <div className="dark:text-white text-gray-700 text-justify">
+              <h5 className="text-xl font-medium mb-2">
                 Overview
               </h5>
-              <p className="text-gray-700 text-base mb-4">{overview}</p>
+              <p className="text-base mb-4">{overview}</p>
+              <p className="text-2xl">
+            Genres: {genres?.map((genre) => ` ${genre.name}`).join(",")}
+          </p>
             </div>
-            <ul className="bg-gray-100 rounded-lg border border-gray-400 text-gray-900">
+            <ul className="dark:text-white text-gray-700 rounded-lg border border-gray-400 mb-6">
+            <li className="px-6 py-2 border-b border-gray-400 w-full rounded-t-lg"><span className={ `text-white tag ${getVoteClass(vote_average)}`}>In Total {vote_count} Votes; Rating: {vote_average?.toFixed(1)}
+            </span></li>
               <li className="px-6 py-2 border-b border-gray-400 w-full rounded-t-lg">
                 {"Release Date : " + release_date}
               </li>
-              <li className={`px-6 py-2 border-b border-gray-400 w-full tag ${getVoteClass(vote_average)}`}>
-                {"Rate : " + vote_average}
-              </li>
+              
               <li className="px-6 py-2 border-b border-gray-400 w-full">
-                {"Total Vote : " + vote_count}
+                {"Popularity: " + popularity}
               </li>
               <li className="px-6 py-2 border-gray-400 w-full rounded-t-lg">
                 <Link
@@ -82,6 +88,7 @@ const MovieDetail = () => {
                   Go Back
                 </Link>
               </li>
+              
             </ul>
           </div>
         </div>
