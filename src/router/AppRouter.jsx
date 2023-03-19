c049b91af4d3d09f30a91813dbc7eef95a9ab77c
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 20:46:20 +0100

    v20

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import ChatRoom from "../pages/ChatRoom";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
+import Profile from "../pages/Profile";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 
@@ -18,6 +19,12 @@ const AppRouter = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/details/:id" element={<PrivateRouter />}>
           <Route path="" element={<MovieDetail />} />
+        </Route>
+        <Route path="/profile" element={<PrivateRouter/>}>
+          <Route path="" element={<Profile/>}/>
+        </Route>
+        <Route path="/chatroom" element={<PrivateRouter/>}>
+          <Route path="" element={<ChatRoom/>}/>
         </Route>
       </Routes>
       <Footer />
