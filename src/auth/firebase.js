3fe663a1d1608f3a33b153907e1de80008a83c40
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 08:58:38 +0100

    V1

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore"
 //* https://firebase.google.com/docs/auth/web/start
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
@@ -19,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const db = getFirestore(app)
