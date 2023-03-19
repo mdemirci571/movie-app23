4b1ff4ff5ce2d982bd454c4f677b87b35fada3ea
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 20:46:42 +0100

    v23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AuthContextProvider from "./context/AuthContext";
+import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
 import MovieContextProvider from "./context/MovieContext";
