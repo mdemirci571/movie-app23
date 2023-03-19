48eef2ee8ee95df362bc7dddb4f0770a122da20d
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 20:45:26 +0100

    v14

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,18 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContextProvider";
 import { MovieContext } from "../context/MovieContext";
 import { toastWarnNotify } from "../helpers/ToastNotify";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
-const POPULAR_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
-  const UPCOMING_API = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`
-  const TOP_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+const POPULAR_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const UPCOMING_API = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`;
+const TOP_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
 const Main = () => {
-  const { movies, loading, errorStatus, getMovies } = useContext(MovieContext);
+  const { movies, loading, getMovies } = useContext(MovieContext);
   const { currentUser } = useContext(AuthContext);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedOption, setSelectedOption] = useState("");
@@ -28,7 +28,7 @@ const Main = () => {
     } else {
       toastWarnNotify("please enter a text");
     }
-    e.target.reset()
+    e.target.reset();
   };
   function handleOptionChange(event) {
     const selectedOption = event.target.value;
@@ -42,24 +42,32 @@ const Main = () => {
     }
   }
   return (
-    <>
-      <form className="flex justify-center p-2" onSubmit={handleSubmit}>
-        <input
-          type="search"
-          className="w-80 h-8 rounded-md p-1 m-2"
-          placeholder="Search a movie..."
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button className="btn-danger-bordered px-6" type="submit">
-          Search
-        </button>
-      </form>
-      <select value={selectedOption} onChange={handleOptionChange} className="px-2 text-center w-25 rounded-lg bg-gray-50 text-gray-900 border border-gray-300 focus:border-danger-600 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:border-danger-600 appearance-none focus:outline-none">
-            <option value="popular">Popular Movies</option>
+    <div className="dark:bg-[#23242a]">
+      <div className="flex justify-center gap-4">
+        <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="w-80 h-8 rounded-md p-1 m-2"
+            placeholder="Search a movie..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="btn-danger-bordered focus:border-gray-600 px-8 dark:bg-gray-900" type="submit">
+            Search
+          </button>
+          <select
+            value={selectedOption}
+            onChange={handleOptionChange}
+            className="btn-danger-bordered cursor-pointer text-center px-4 rounded-lg focus:border-gray-600 focus:outline-none dark:bg-gray-900"
+          >
+            
+            {/* <option defaultValue={""}>Choose Movies</option> */}
+            <option value="popular">Choose Movies</option>
             <option value="upcoming">Upcoming Movies</option>
             <option value="top-rated">Top Rated Movies</option>
-        </select>
-      <div className="flex justify-center flex-wrap">
+          </select>
+        </form>
+      </div>
+      <div className="flex justify-center flex-wrap mb-14">
         {loading ? (
           <div
             className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
@@ -71,7 +79,7 @@ const Main = () => {
           movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
         )}
       </div>
-    </>
+    </div>
   );
 };
 
