fbab0f93aff833a3ca8b2b98f442f7047ae895c2
muhammeddemirci <mhdemirci571@gmail.com>
2023-03-19 09:15:47 +0100

    moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,6 +18,14 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
       return "red";
     }
   };
+  const handleRedirect = () => {
+    if (currentUser) {
+      navigate(`/details/${movie.id}`) 
+    }else{
+      toast.error("Pleage log in for more details!")
+      (navigate(`/details/${movie.id}`))
+    }
+  }
   return (
     <div
       className="movie"
@@ -41,6 +49,15 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
         <h2>Overview</h2>
         <p>{overview}</p>
       </div>
+      <button
+                  type="button"
+                  className="inline-block rounded bg-danger px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)]"
+                  data-te-ripple-init=""
+                  data-te-ripple-color="light"
+                  onClick={handleRedirect}>
+                  More Details
+                </button>
+
     </div>
   );
 };
